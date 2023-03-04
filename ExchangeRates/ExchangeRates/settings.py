89d8e4d1584e6417e06c9fd28b00b6531ba59379
Infarni <archdroider@proton.me>
2023-03-04 16:42:48 +0200

    Default rendered

--- ExchangeRates/ExchangeRates/settings.py
+++ ExchangeRates/ExchangeRates/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 20
+}
